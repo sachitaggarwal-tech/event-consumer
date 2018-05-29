7d5a996e2b7f4e5d664e7320561f7d65c77a047f
Sachit Aggarwal <sachit.aggarwal@sap.com>
2018-05-29 10:06:49 +0530

    changes
    Change-Id: I7b5400ffef8c9414b3e4f161a4def39780ea3cdc
    Signed-off-by: Sachit Aggarwal <sachit.aggarwal@sap.com>

--- src/test/java/com/example/zintegration/tests/ITMessageServiceTest.java
+++ src/test/java/com/example/zintegration/tests/ITMessageServiceTest.java
@@ -1,12 +1,34 @@
-//package com.example.zintegration.tests;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class ITMessageServiceTest {
-//
-//	@Test
-//	public void testMessageServiceEndToEnd() {
-//		Assert.assertTrue(false);
-//	}
-//}
+package com.example.zintegration.tests;
+
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.example.base.utils.APIHandler;
+
+public class ITMessageServiceTest {
+
+	private static final String TEST_URL = "https://persephone-event-consumer.cfapps.eu10.hana.ondemand.com/messages";
+
+	@Test
+	public void testSendRecieveMessage() throws InterruptedException {
+		APIHandler handler = new APIHandler();
+		String response = handler.executeGetCall(TEST_URL, Collections.emptyMap());
+		Assert.assertTrue("After fresh deploy messages list not empty", "[]".equals(response));
+		
+		//send message
+		String testMessage = "{\"EVENT_PAYLOAD\": {\"KEY\": [{\"BUSINESSPARTNER\": \"123\"}]}}";
+		int responseCode = handler.executePostCall(TEST_URL, testMessage, Collections.emptyMap());
+		Assert.assertTrue("Sending message to queue failed",responseCode == 202);
+		
+		//wait
+		Thread.sleep(5000);
+		
+		//read message
+		response = handler.executeGetCall(TEST_URL, Collections.emptyMap());
+		Assert.assertTrue("After fresh deploy messages list not empty", response.contains("123"));
+	
+	}
+	
+}
